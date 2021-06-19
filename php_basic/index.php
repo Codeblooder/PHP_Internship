6f525e16d73f80c031868c6943949c93fc56e20d
Codeblooder <61426578+Codeblooder@users.noreply.github.com>
2021-06-19 16:12:01 +0530

    Add files via upload

---
+++ php_basic/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+  <meta name="description" content="Free Web tutorials">
+  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="author" content="John Doe">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - Index</title>
+
+</head>
+<body>
+  <!-- Basic HTML -->
+  <h2>Follow each link to the page example</h2>
+  <ul>
+  <li><a href="array.php">array</a></li>
+  <li><a href="string
+    .php">Simple stirng manipulation</a></li>
+  <li><a href="Date_Time_Mani.php">Date and Time manipulation</a></li>
+  </ul>
+  <br/>
+  <h1>HEYY</h1>
+  <br/>
+
+<?php
+ echo "Rukhsar Bombaywala  ";
+ echo '</br>';
+ echo 300+20;
+?>
+
+<?php
+ $name = 'Rukhsar Bombaywala';
+ echo $name;
+ echo '</br>';
+ echo '<h2>My Name is: '.$name.'</h2>';
+?>
+</body>
+</html>
